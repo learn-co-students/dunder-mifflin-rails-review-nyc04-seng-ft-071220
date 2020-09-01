549630d3ce3aa92e362496d9d252368fd00e1d06
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-01 15:36:33 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+    def index
+        @dogs=Dog.all
+    end
+
+    def new
+        @dog=Dog.new
+    end
+
+    def create
+        @dog=Dog(dog_param)
+        redirect_to dog_path(@dog)
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def home
+        
+    end
+
+    private
+    def dog_param
+        params.require(:dog).permit(:name,:breed,:age)
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,48 @@
 class EmployeesController < ApplicationController
+    
+    def index
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def create
+        @employee = Employee.create(employee_param)
+        if @employee.valid?
+            redirect_to students_path
+        # else, render the form back and save the errors
+        else
+            flash[:errors] = @employee.errors.full_messages
+            redirect_to employees_path
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_param)
+        redirect_to employee_path(@employee)
+    end
+
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy
+        redirect_to employees_path
+    end
+
+    private
+
+    def employee_param
+        params.require(:employee).permit(:first_name,:last_name,:alias,:title,:office,:img_url,:dog_id)
+    end
+
 end
