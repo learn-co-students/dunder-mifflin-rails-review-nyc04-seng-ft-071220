c731b5cb8b393772020b7f17cd660974924b1d35
ChristyTropila <tropila04@gmail.com>
2020-09-01 00:10:37 -0400

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,36 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees=Employee.all
+    end
+    
+    def show
+        @employee=Employee.find(params[:id])
+    end
+
+    def new
+      @employee=Employee.new
+      @dog=Dog.all
+        
+    end
+    
+    def create
+        @employee=Employee.new(employee_params)
+        params[:employee]['img_url'].each do |img|
+            @employee.img_url=img
+            @employee.save
+        end
+    end
+    
+    def edit 
+    end
+
+    def update 
+    end
+
+    def destroy
+    end
+    
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
 end
